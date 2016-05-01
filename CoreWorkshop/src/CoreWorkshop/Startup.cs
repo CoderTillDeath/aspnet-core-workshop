fe9bf6ccbd26e868142369ccd35b7a7b87ba4618
Benson Chu <bensonchu457@gmail.com>
2016-05-01 18:33:18 -0500

    MVC Setup

--- CoreWorkshop/src/CoreWorkshop/Startup.cs
+++ CoreWorkshop/src/CoreWorkshop/Startup.cs
@@ -15,6 +15,7 @@ namespace CoreWorkshop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,6 +26,13 @@ namespace CoreWorkshop
             app.UseDeveloperExceptionPage();
 
             app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "Default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
 
         // Entry point for the application.
