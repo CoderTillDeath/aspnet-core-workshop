3f12ea55fca5fc73f0356b3cc5a905a9f465edb9
Benson Chu <bensonchu457@gmail.com>
2016-05-03 14:20:28 -0500

    Tag Helpers and custom Tag Helper

---
+++ CoreWorkshop/src/CoreWorkshop/TagHelpers/MarkdownTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace CoreWorkshop.TagHelpers
+{
+    // You may need to install the Microsoft.AspNet.Razor.Runtime package into your project
+    // [HtmlTargetElement("tag-name")]  // Manually set tag name
+    public class MarkdownTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContent = await output.GetChildContentAsync();
+            output.Content.SetHtmlContent(CommonMark.CommonMarkConverter.Convert(childContent.GetContent()));
+            output.TagName = null;
+        }
+    }
+}
