6f8521a191694f78a0c00bd0fc0b89bea777009c
Benson Chu <bensonchu457@gmail.com>
2016-05-03 20:51:55 -0500

    Created Database, corrected error in startup.cs

--- CoreWorkshop/src/CoreWorkshop/Startup.cs
+++ CoreWorkshop/src/CoreWorkshop/Startup.cs
@@ -17,7 +17,7 @@ namespace CoreWorkshop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNet5.NewDb;Trusted_Connection=True;";
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=CoreWorkshop;Trusted_Connection=True;";
 
             services.AddEntityFramework()
                 .AddSqlServer()
