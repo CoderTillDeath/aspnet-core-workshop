572b34e9b45bc65c938a1f1c3bb18ef2eb6db742
Benson Chu <bensonchu457@gmail.com>
2016-05-03 20:49:11 -0500

    Startup.cs modifications, error in model.cs

--- CoreWorkshop/src/CoreWorkshop/Startup.cs
+++ CoreWorkshop/src/CoreWorkshop/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Data.Entity;
+using CoreWorkshop.Models;
 
 namespace CoreWorkshop
 {
@@ -15,6 +17,12 @@ namespace CoreWorkshop
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=(localdb)\mssqllocaldb;Database=EFGetStarted.AspNet5.NewDb;Trusted_Connection=True;";
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<BloggingContext>(options => options.UseSqlServer(connection));
+
             services.AddMvc();
         }
 
